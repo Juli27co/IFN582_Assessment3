12a927c3f84019a247c02983b0caeb2e63955a37
Marina <marina.takeda@connect.qut.edu.au>
2025-10-09 20:47:35 +1000

    test

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
-#Marina updated
 from project import create_app
-#   this is just for test
-# this is also the test2
+
 if __name__ == '__main__':
     app = create_app()
     app.run(debug = True, port=8888)
