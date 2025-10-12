01371b7edfbc1e5adb83d1f650e81f008719451a
Atcharanan-s <atcharanan.siriployngam@qut.edu.au>
2025-10-12 13:38:03 +1000

    1st models updated

--- project/models.py
+++ project/models.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from enum import Enum
+
 
 @dataclass
 class Image:
@@ -18,7 +20,7 @@ class Service:
     addOns: str
     price: float
 
-@classdata
+@dataclass
 class User:
     id: str
     email: str
@@ -26,3 +28,28 @@ class User:
     telephone: str
     firstName: str
     lastName: str
+
+@dataclass
+class AvailabilityStatus(Enum):
+    WEEKDAYS_ONLY = "Weekdays only"
+    WEEKENDS_ONLL = "Weekends only"
+    SHORT_NOTICE_BOOKING = "Short notice booking"
+
+@dataclass
+class Photographer:
+    id: str
+    email: str
+    password: str
+    telephone: str
+    firstName: str
+    lastName: str
+    bioDescription: str 
+    location: str
+    availability: AvailabilityStatus
+    rating: float
+
+@dataclass
+class Portfolio:
+    id: str
+    photographer: Photographer
+
